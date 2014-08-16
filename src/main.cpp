a6a8bf488b58f90703931cc3e2331fe651d614be
Elvis Stansvik <elvstone@gmail.com>
2014-08-16 14:27:13 +0200

    Comment fix.
    Missing parameter name for @p.

--- src/main.cpp
+++ src/main.cpp
@@ -350,8 +350,8 @@ static QStringList supportedFormats()
  * Returns the template specified by @p name.
  *
  * The @p name parameter may be either a template file name, or the name of a
- * supported format ("html", "docbook", ...). If an error occured, @p is set to
- * point to an error message and QString() returned.
+ * supported format ("html", "docbook", ...). If an error occured, @p error is
+ * set to point to an error message and QString() returned.
  */
 static QString readTemplate(const QString &name, std::string *error)
 {
