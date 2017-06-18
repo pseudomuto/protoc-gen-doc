dc63c08583759110c462e6b3499426f324ec9346
David Muto (pseudomuto) <david.muto@gmail.com>
2017-06-18 10:34:05 -0400

    Handling proto3 files

---
+++ parser/comments.go
@@ -0,0 +1,58 @@
+package parser
+
+import (
+	"github.com/golang/protobuf/protoc-gen-go/descriptor"
+	"strconv"
+	"strings"
+)
+
+type commentContainer interface {
+	getPath() string
+	setComment(comment string)
+}
+
+type commentExtractor struct {
+	comments map[string]string
+}
+
+func newCommentExtractor(fd *descriptor.FileDescriptorProto) *commentExtractor {
+	comments := make(map[string]string)
+
+	for _, loc := range fd.GetSourceCodeInfo().GetLocation() {
+		if loc.LeadingComments == nil && loc.TrailingComments == nil {
+			continue
+		}
+
+		var p []string
+		for _, n := range loc.GetPath() {
+			p = append(p, strconv.Itoa(int(n)))
+		}
+
+		comments[strings.Join(p, ",")] = strings.Join(
+			[]string{loc.GetLeadingComments(), loc.GetTrailingComments()},
+			"\n\n",
+		)
+	}
+
+	return &commentExtractor{comments}
+}
+
+func (ce *commentExtractor) extractComments(containers ...commentContainer) {
+	for _, container := range containers {
+		container.setComment(ce.commentForPath(container.getPath()))
+	}
+}
+
+func (ce *commentExtractor) commentForPath(path string) string {
+	return scrubComment(ce.comments[path])
+}
+
+func scrubComment(s string) string {
+	lines := strings.Split(s, "\n")
+
+	for idx, line := range lines {
+		lines[idx] = strings.Trim(line, " /\n*")
+	}
+
+	return strings.Trim(strings.Join(lines, "\n"), "\n")
+}
---
+++ test/cmd/gen_fixtures/main.go
@@ -0,0 +1,21 @@
+// Functions like a normal code generator, except that it won't output anything. Is it used to store the
+// CodeGeneratorRequest sent in by protoc for use in unit tests.
+//
+// Example:
+//   protoc --plugin=protoc-gen-doc=./gen_fixtures --doc_out=. fixtures/proto3/*.proto
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+	data, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatalf("Could not read contents from stdin")
+	}
+
+	ioutil.WriteFile("fixtures/proto3_generator_request.dat", data, 0666)
+}
