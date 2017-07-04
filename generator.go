d4306e651ffbf9e8beaab0808694da9d69df8d33
David Muto (pseudomuto) <david.muto@gmail.com>
2017-07-03 20:10:41 -0400

    Move test/cmd to build/cmd

--- generator.go
+++ generator.go
@@ -1,6 +1,6 @@
 package protoc_gen_doc
 
-//go:generate go build ./test/cmd/gen_fixtures
+//go:generate go build ./build/cmd/gen_fixtures
 //go:generate protoc --plugin=protoc-gen-doc=./gen_fixtures --doc_out=. fixtures/Booking.proto fixtures/Vehicle.proto
 //go:generate rm gen_fixtures
 
