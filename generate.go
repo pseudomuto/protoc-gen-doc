ecdb803e73797bb9310b34e3d458ed15cc498184
David Muto (pseudomuto) <david.muto@gmail.com>
2017-07-21 19:36:44 -0400

    mv ./templates ./resources

--- generate.go
+++ generate.go
@@ -4,4 +4,4 @@ package protoc_gen_doc
 //go:generate protoc --plugin=protoc-gen-doc=./gen_fixtures --doc_out=./test test/fixtures/Booking.proto test/fixtures/Vehicle.proto
 //go:generate rm gen_fixtures
 
-//go:generate go run build/cmd/resources/main.go -in templates -out resources.go -pkg protoc_gen_doc
+//go:generate go run build/cmd/resources/main.go -in resources -out resources.go -pkg protoc_gen_doc
