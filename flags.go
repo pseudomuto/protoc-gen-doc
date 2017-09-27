05c82d9fb06df3f15b7872e3060fc50ff120f59d
David Muto (pseudomuto) <david.muto@gmail.com>
2017-09-27 10:42:15 -0400

    Add support for -help, -h, and -version flags

---
+++ flags.go
@@ -0,0 +1,74 @@
+package gendoc
+
+import (
+	"flag"
+	"fmt"
+	"io"
+)
+
+// Flags contains details about the CLI invocation of protoc-gen-doc
+type Flags struct {
+	appName     string
+	flagSet     *flag.FlagSet
+	err         error
+	showHelp    bool
+	showVersion bool
+	writer      io.Writer
+}
+
+// Code returns the status code to exit with after handling the supplied flags
+func (f *Flags) Code() int {
+	if f.err != nil {
+		return 1
+	}
+
+	return 0
+}
+
+// HasMatch returns whether or not the supplied args are matches. For example, passing `--help` will match, or some
+// unknown parameter, but passing nothing will not.
+func (f *Flags) HasMatch() bool {
+	return f.ShowHelp() || f.ShowVersion()
+}
+
+// ShowHelp determines whether or not to show the help message
+func (f *Flags) ShowHelp() bool {
+	return f.err != nil || f.showHelp
+}
+
+// ShowVersion determines whether or not to show the version message
+func (f *Flags) ShowVersion() bool {
+	return f.showVersion
+}
+
+// PrintHelp prints the usage string including all flags to the `io.Writer` that was supplied to the `Flags` object.
+func (f *Flags) PrintHelp() {
+	fmt.Fprintf(f.writer, "Usage of %s:\n\n", f.appName)
+	fmt.Fprintf(f.writer, "FLAGS\n")
+	f.flagSet.PrintDefaults()
+}
+
+// PrintVersion prints the version string to the `io.Writer` that was supplied to the `Flags` object.
+func (f *Flags) PrintVersion() {
+	fmt.Fprintf(f.writer, "%s version %s\n", f.appName, VERSION)
+}
+
+// ParseFlags parses the supplied options are returns a `Flags` object to the caller.
+//
+// Parameters:
+//   * `w` - the `io.Writer` to use for printing messages (help, version, etc.)
+//   * `args` - the set of args the program was invoked with (typically `os.Args`)
+func ParseFlags(w io.Writer, args []string) *Flags {
+	f := Flags{appName: args[0], writer: w}
+
+	f.flagSet = flag.NewFlagSet(args[0], flag.ContinueOnError)
+	f.flagSet.BoolVar(&f.showHelp, "help", false, "Show this help message")
+	f.flagSet.BoolVar(&f.showVersion, "version", false, fmt.Sprintf("Print the current version (%v)", VERSION))
+	f.flagSet.SetOutput(w)
+
+	// prevent showing help on parse error
+	f.flagSet.Usage = func() {}
+
+	f.err = f.flagSet.Parse(args[1:])
+	return &f
+}
---
+++ flags_test.go
@@ -0,0 +1,88 @@
+package gendoc_test
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/pseudomuto/protoc-gen-doc"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type FlagsTest struct {
+	suite.Suite
+}
+
+func TestFlags(t *testing.T) {
+	suite.Run(t, new(FlagsTest))
+}
+
+func (assert *FlagsTest) TestCode() {
+	f := gendoc.ParseFlags(nil, []string{"app", "-help"})
+	assert.Equal(0, f.Code())
+
+	f = gendoc.ParseFlags(nil, []string{"app", "-whoawhoawhoa"})
+	assert.Equal(1, f.Code())
+}
+
+func (assert *FlagsTest) TestHasMatch() {
+	f := gendoc.ParseFlags(nil, []string{"app", "-help"})
+	assert.True(f.HasMatch())
+
+	f = gendoc.ParseFlags(nil, []string{"app", "-version"})
+	assert.True(f.HasMatch())
+
+	f = gendoc.ParseFlags(nil, []string{"app", "-watthewhat"})
+	assert.True(f.HasMatch())
+
+	f = gendoc.ParseFlags(nil, []string{"app"})
+	assert.False(f.HasMatch())
+}
+
+func (assert *FlagsTest) TestShowHelp() {
+	f := gendoc.ParseFlags(nil, []string{"app", "-help"})
+	assert.True(f.ShowHelp())
+
+	f = gendoc.ParseFlags(nil, []string{"app", "-version"})
+	assert.False(f.ShowHelp())
+}
+
+func (assert *FlagsTest) TestShowVersion() {
+	f := gendoc.ParseFlags(nil, []string{"app", "-version"})
+	assert.True(f.ShowVersion())
+
+	f = gendoc.ParseFlags(nil, []string{"app", "-help"})
+	assert.False(f.ShowVersion())
+}
+
+func (assert *FlagsTest) TestPrintHelp() {
+	buf := new(bytes.Buffer)
+
+	f := gendoc.ParseFlags(buf, []string{"app"})
+	f.PrintHelp()
+
+	result := buf.String()
+	assert.Contains(result, "Usage of app:\n\n")
+	assert.Contains(result, "FLAGS\n")
+	assert.Contains(result, "-help")
+	assert.Contains(result, "-version")
+}
+
+func (assert *FlagsTest) TestPrintVersion() {
+	buf := new(bytes.Buffer)
+
+	f := gendoc.ParseFlags(buf, []string{"app"})
+	f.PrintVersion()
+
+	// Normally, I'm not a fan of using constants like this in tests. However, having this break everytime the version
+	// changes is kinda poop, so I've used VERSION here.
+	assert.Equal(fmt.Sprintf("app version %s\n", gendoc.VERSION), buf.String())
+}
+
+func (assert *FlagsTest) TestInvalidFlags() {
+	buf := new(bytes.Buffer)
+
+	f := gendoc.ParseFlags(buf, []string{"app", "-wat"})
+	assert.Contains(buf.String(), "flag provided but not defined: -wat\n")
+	assert.True(f.HasMatch())
+	assert.True(f.ShowHelp())
+}
