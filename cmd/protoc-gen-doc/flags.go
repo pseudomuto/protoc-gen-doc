910fc8070945ea8ff523adf9ad56325ad5ed6ae2
William Chang <willdotchang@gmail.com>
2022-08-18 16:18:21 -0400

    Updated documentation and readme.

--- cmd/protoc-gen-doc/flags.go
+++ cmd/protoc-gen-doc/flags.go
@@ -24,6 +24,9 @@ protoc --doc_out=. --doc_opt=custom.tmpl,docs.txt protos/*.proto
 EXAMPLE: Generate docs relative to source protos
 protoc --doc_out=. --doc_opt=html,index.html,source_relative protos/*.proto
 
+EXAMPLE: Generate docs relative to source protos, one output file per input file
+protoc --doc_out=. --doc_opt=html,html,source_relative,separate_files protos/*.proto
+
 See https://github.com/pseudomuto/protoc-gen-doc for more details.
 `
 
