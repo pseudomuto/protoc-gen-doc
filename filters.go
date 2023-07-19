13ec5ee3a6e0622d9c647f815cf68c31b6904403
w <119905976+wquark@users.noreply.github.com>
2023-07-19 13:24:57 -0500

    slugify anchor
    - vaguely attempts to match npm @sindresorhus/slugify

--- filters.go
+++ filters.go
@@ -5,6 +5,8 @@ import (
 	"html/template"
 	"regexp"
 	"strings"
+
+  "github.com/gosimple/slug"
 )
 
 var (
@@ -12,6 +14,13 @@ var (
 	spacePattern        = regexp.MustCompile("( )+")
 	multiNewlinePattern = regexp.MustCompile(`(\r\n|\r|\n){2,}`)
 	specialCharsPattern = regexp.MustCompile(`[^a-zA-Z0-9_-]`)
+
+	capDigitsRegex      = regexp.MustCompile(`([A-Z]{2,})(\d+)`)
+	lowerCapDigitsRegex = regexp.MustCompile(`([a-z\d]+)([A-Z]{2,})`)
+	lowerUpperRegex     = regexp.MustCompile(`([a-z\d])([A-Z])`)
+  // `[a-rt-z]` matches all lowercase characters except `s`.
+  // This avoids matching plural acronyms like `APIs`.
+	pluralAcronymsRegex = regexp.MustCompile(`([A-Z]+)([A-Z][a-rt-z\d]+)`)
 )
 
 // PFilter splits the content by new lines and wraps each one in a <p> tag.
@@ -38,7 +47,16 @@ func NoBrFilter(content string) string {
 	return strings.Join(paragraphs, "\n\n")
 }
 
-// AnchorFilter replaces all special characters with URL friendly dashes
+func decamelize(input string) string {
+	// Replace the matched patterns with spaces to decamelize the string.
+	input = capDigitsRegex.ReplaceAllString(input, "$1 $2")
+	input = lowerCapDigitsRegex.ReplaceAllString(input, "$1 $2")
+	input = lowerUpperRegex.ReplaceAllString(input, "$1 $2")
+	input = pluralAcronymsRegex.ReplaceAllString(input, "$1 $2")
+
+	return input
+}
+
 func AnchorFilter(str string) string {
-	return specialCharsPattern.ReplaceAllString(strings.ReplaceAll(str, "/", "_"), "-")
+  return slug.Make(decamelize(str))
 }
--- go.mod
+++ go.mod
@@ -21,6 +21,8 @@ require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/google/uuid v1.1.2 // indirect
+	github.com/gosimple/slug v1.13.1 // indirect
+	github.com/gosimple/unidecode v1.0.1 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
