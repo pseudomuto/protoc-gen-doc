610ba0f451c1b3d01cb49898d0d18663cbdc47a2
cz-cube <149609894+cz-cube@users.noreply.github.com>
2024-01-17 12:36:34 -0600

    Update AnchorFilter for GitBook (#1)

--- filters.go
+++ filters.go
@@ -5,22 +5,13 @@ import (
 	"html/template"
 	"regexp"
 	"strings"
-
-  "github.com/gosimple/slug"
 )
 
 var (
 	paraPattern         = regexp.MustCompile(`(\n|\r|\r\n)\s*`)
 	spacePattern        = regexp.MustCompile("( )+")
 	multiNewlinePattern = regexp.MustCompile(`(\r\n|\r|\n){2,}`)
-	specialCharsPattern = regexp.MustCompile(`[^a-zA-Z0-9_-]`)
-
-	capDigitsRegex      = regexp.MustCompile(`([A-Z]{2,})(\d+)`)
-	lowerCapDigitsRegex = regexp.MustCompile(`([a-z\d]+)([A-Z]{2,})`)
-	lowerUpperRegex     = regexp.MustCompile(`([a-z\d])([A-Z])`)
-  // `[a-rt-z]` matches all lowercase characters except `s`.
-  // This avoids matching plural acronyms like `APIs`.
-	pluralAcronymsRegex = regexp.MustCompile(`([A-Z]+)([A-Z][a-rt-z\d]+)`)
+	specialCharsPattern = regexp.MustCompile(`[^a-zA-Z0-9_.-]`)
 )
 
 // PFilter splits the content by new lines and wraps each one in a <p> tag.
@@ -47,16 +38,9 @@ func NoBrFilter(content string) string {
 	return strings.Join(paragraphs, "\n\n")
 }
 
-func decamelize(input string) string {
-	// Replace the matched patterns with spaces to decamelize the string.
-	input = capDigitsRegex.ReplaceAllString(input, "$1 $2")
-	input = lowerCapDigitsRegex.ReplaceAllString(input, "$1 $2")
-	input = lowerUpperRegex.ReplaceAllString(input, "$1 $2")
-	input = pluralAcronymsRegex.ReplaceAllString(input, "$1 $2")
-
-	return input
-}
-
 func AnchorFilter(str string) string {
-  return slug.Make(decamelize(str))
+	res := strings.ReplaceAll(str, "/", "_")
+	res = specialCharsPattern.ReplaceAllString(res, "-")
+	res = strings.ToLower(res)
+	return res
 }
